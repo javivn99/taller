04534c3befea77832437bf11872dfa16b5e2ab73
javivn99 <javiyosoyyo@gmail.com>
2020-12-04 19:41:26 +0100

    ya va elmenu guarro de citas

--- admin/citas.php
+++ admin/citas.php
@@ -5,13 +5,12 @@ include 'head.php';
 
 print '
 
-    <br><h2>Seleccione una opcion</h2><br>
+    <br><h2 class="seleccion">Seleccione una opcion</h2><br>
     <div class="menu_citas">
-        <ul>
-            <li>Añadir cita</li>
-            <li>Cancelar cita</li>
-            <li>Modificar cita</li>
-        </ul>
+            <li><a href="aalta.php">Añadir cita</a></li>
+            <li><a href="aborrar.php">Cancelar cita</a></li>
+            <li><a href="amodificar.php">Modificar cita</a></li>
+            <li><a href="aconsultar.php">Consultar</a></li>
     </div>
 
 
